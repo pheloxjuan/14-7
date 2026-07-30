9bea24a6b6e2a3e4ff9439e25210eaf74ed11857
pheloxjuan <304538559+pheloxjuan@users.noreply.github.com>
2026-07-30 10:06:36 -0300

    Cargar stock completo por bloques

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'phelox-pwa-2026-07-30-v3';
+const CACHE_NAME = 'phelox-pwa-2026-07-30-v4';
 const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/phelox-logo-192-v2.png', '/phelox-logo-512-v2.png', '/apple-touch-icon-v2.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
